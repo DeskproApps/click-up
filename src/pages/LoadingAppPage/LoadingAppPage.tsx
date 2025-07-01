21947ceab63e452399daee93fff81c2143cec72b
OJ Abba <phavorabba@gmail.com>
2025-07-01 16:51:18 +0100

    [Chore] Introduce errors 💥

--- src/pages/LoadingAppPage/LoadingAppPage.tsx
+++ src/pages/LoadingAppPage/LoadingAppPage.tsx
@@ -1,17 +1,38 @@
 import { LoadingSpinner, useDeskproElements } from "@deskpro/app-sdk";
-import { useCheckAuth } from "./hooks";
-import type { FC } from "react";
+import { useEffect, useState, type FC } from "react";
+import { Button } from "@deskpro/deskpro-ui";
 
 const LoadingAppPage: FC = () => {
-  useCheckAuth();
 
   useDeskproElements(({ registerElement, clearElements }) => {
     clearElements();
     registerElement("refresh", { type: "refresh_button" });
   });
 
+  const [errorState, setErrorState] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (errorState === "left") {
+      throw new Error("hello")
+    }
+
+    if (errorState === "right") {
+      throw "HI"
+    }
+  }, [errorState])
+
   return (
-    <LoadingSpinner/>
+    <>
+      <Button
+        text="Left Error"
+        onClick={() => { setErrorState("left") }}
+      />
+      <Button
+        text="Right Error"
+        onClick={() => { setErrorState("right") }}
+      />
+      <LoadingSpinner />
+    </>
   );
 };
 
